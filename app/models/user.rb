542dbb249e5a1d88d5a59067030b7a8be2e429bd
yagince <straitwalk@gmail.com>
2013-05-11 13:13:28 +0900

    deleted unused files & add User#name validation spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email
+
+  validates :name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_user) { User.new(name: "test-user", email: "hoge@gmail.com") }
+
+  describe "validations" do
+    describe "name" do
+      before do
+        @noname_user = valid_user.tap{|u|
+          u.name = ""
+          u.save
+        }
+      end
+      it "required" do
+        expect(@noname_user).not_to be_valid
+      end
+    end
+  end
 end
