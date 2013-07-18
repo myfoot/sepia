80d372f9b5a1effbcea0f5cf847851ec685a3641
yagince <straitwalk@gmail.com>
2013-07-18 21:25:12 +0900

    modified provider twit_pic -> twitter

--- app/workers/photo_crawler.rb
+++ app/workers/photo_crawler.rb
@@ -5,8 +5,8 @@ class PhotoCrawler
 
   def perform(client_name, access_token_id)
     client = client_name.constantize.new(AccessToken.find(access_token_id))
-    last_photos = Photo.where(provider: client.provider, user_id: client.user_id).order("posted_at DESC").last
-    client.photos(last_photos.try(:posted_at)).each{|photo|
+    last_photo = Photo.where(provider: client.provider, user_id: client.user_id).order("posted_at DESC").first
+    client.photos(last_photo.try(:posted_at)).each{|photo|
       photo.save # TODO: save失敗時の対応は別途考える
     } 
   end
