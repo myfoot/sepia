3e2c7c44ebc765c964b2281ae50c8a783eecfaaf
yagince <straitwalk@gmail.com>
2013-08-04 00:57:10 +0900

    Album作成が出来るよ (*´∀｀*)

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,7 @@
 class Album < ActiveRecord::Base
   belongs_to  :user
-  has_many :albums_photos, class_name: 'AlbumsPhotos', inverse_of: :album
+  has_many :albums_photos, class_name: 'AlbumsPhotos', inverse_of: :album, dependent: :destroy
   has_many :photos, through: :albums_photos
+
+  validates :name, presence: true
 end
---
+++ app/views/albums/create.json.jbuilder
@@ -0,0 +1,10 @@
+if @album.valid?
+  json.set! :result, :success
+  json.id @album.id
+else
+  response.status = 400
+  json.set! :result, :error
+  json.errors @album.errors do |key|
+    json.set! key, @album.errors[key]
+  end
+end
